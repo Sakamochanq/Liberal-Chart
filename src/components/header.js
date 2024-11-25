82a796a976fa739ed16f5f11f0e76cdcec6a8581
Sakamochanq <Sakamochan.q@outlook.jp>
2024-11-26 01:14:37 +0900

    add: Header-Componentの作成

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const header = () => {
+  return (
+    <header>
+      <h3 class="nav-logo">Ranked-Liberal</h3>
+      <nav>
+        <ul class="menu">
+          <li class="menu-item"><a href="#">Home</a></li>
+          <li class="menu-item"><a href="#">Help</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default header
