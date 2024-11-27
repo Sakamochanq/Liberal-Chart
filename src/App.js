76bea8bb760a3b123c46f747c81d40833f88b373
Sakamochanq <Sakamochan.q@outlook.jp>
2024-11-28 01:55:02 +0900

    feat: json読み込み機能の実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Explain from './components/Explain';
 import './App.css';
+import Lists from './components/Lists';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Explain/>
+      <Lists/>
     </div>
   );
 }
