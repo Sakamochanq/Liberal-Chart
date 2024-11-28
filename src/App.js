78c4f0e60839902013636d25ddd96e866f3d609e
Sakamochanq <Sakamochan.q@outlook.jp>
2024-11-28 17:18:59 +0900

    bug: Componentの削除

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Header from './components/Header';
-import Lists from './components/Lists';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Lists/>
     </div>
   );
 }
