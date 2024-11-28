d0054a091895231637d219ac1dbb75d6a499e141
Sakamochanq <Sakamochan.q@outlook.jp>
2024-11-28 17:20:45 +0900

    fix: 読み込みエラーの修正

---
+++ src/components/Explain.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Explain = () => {
+  return (
+    <div class="explain-container">
+      <div class="explain-header">
+        <h2 style={{marginBottom: '30px'}}>* Ranked-Liberal とは？</h2>
+        <p>
+          金沢工業大学のリベラルアーツ系科目の<b> 早見表 </b>です。履修登録の際の参考にしましょう。
+          このサイトは<b> Sakamochanq </b> によって作成・管理されているため、情報の正確性は保証されません。
+          正規の情報は金沢工業大学の公式サイト又は、履修ガイドを参照してください。
+        </p>
+        <hr style={{width: '100%', margin: '50px 0px 50px 0px'}}></hr>
+      </div>
+    </div>
+  )
+}
+export default Explain
