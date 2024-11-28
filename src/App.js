1de7e77f2f4bfb1438bbea8bdb5659e9e702003f
Sakamochanq <Sakamochan.q@outlook.jp>
2024-11-28 18:13:26 +0900

    fix: Deployバグの修正

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import Explain from "./components/Explain";
+import Lists from "./components/Lists"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Explain/>
+      <Lists/>
     </div>
   );
 }
