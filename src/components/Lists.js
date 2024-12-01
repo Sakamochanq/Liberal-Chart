cdeb505a3872ff6fc92255da0c61aa6c8812b88f
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-01 22:44:43 +0900

    remove: 不適切かつ差別的用語の削除

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -14,10 +14,10 @@ const Lists = () => {
     <div class="list-container">
       {subjects.map((subject, index) => (
         <div class="list-wrapper" key={index}>
-          <p style={{marginBottom: "10px"}} class="rank-id">* {subject['rank-id']} 位</p>
+          {/* <p style={{marginBottom: "10px"}} class="rank-id">ID：{subject['rank-id']}</p> */}
           <h3 style={{marginTop: "20px", marginBottom:"20px"}} class="name">{subject.name}</h3>
+          <p style={{marginBottom: "10px"}} class="inside-job">試験：<b>{subject['inside-job'] ? '有' : '無'}</b></p>
           <p style={{marginBottom: "10px"}} class="degree">難易度：{subject.degree}</p>
-          <p style={{marginBottom: "10px"}} class="inside-job">内職：<b>{subject['inside-job'] ? '可' : '不可'}</b></p>
           <p style={{marginBottom: "10px"}} class="description">概要：{subject.description}</p>
         </div>
       ))}
