ee80e75a1157ea24b5340b61f00f8466db6ddd6c
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-02 21:28:11 +0900

    add: SearchQueryの追加

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,18 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const Lists = () => {
   const [subjects, setSubjects] = useState([]);
+  const [search, setSearch] = useState([]);
+  const ref = useRef();
+
+  const handleSearch = () => {
+    setSearch(
+      subjects.filter((subject) => subject.name.toLowerCase().includes(ref.current.value.toLowerCase()))
+    );
+  }
 
   useEffect(() => {
     fetch('./assets/table.json')
       .then(response => response.json())
-      .then(data => setSubjects(data))
+      .then(data => {
+        setSubjects(data);
+        //初期状態では全ての科目を表示
+        setSearch(data);
+      })
       .catch(error => console.error('Error loading the JSON file:', error));
   }, []);
 
   return (
     <div className="list-container">
-      {subjects.map((subject, index) => (
+      <input ref={ref} type="text" placeholder="科目名で検索" onChange={handleSearch} />
+      {search.map((subject, index) => (
         <div className="list-wrapper" key={index}>
           {/* <p style={{marginBottom: "10px"}} class="rank-id">ID：{subject['rank-id']}</p> */}
           <h3 style={{marginTop: "10px", marginBottom:"20px"}} className="name">{subject.name}</h3>
