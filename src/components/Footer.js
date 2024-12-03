9825867de71ee7de6d1cf91d81efefff1ade47aa
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-04 00:11:38 +0900

    fix: footerのサイズ変更

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ export const Footer = () => {
     <footer>
         <div className="footer-nav">
             <p>&copy; {new Date().getFullYear()} <b className="bold-text">Sakamochanq</b></p>
-            <p>Powered by React</p>
             <p>Data and resources provided by <b className="bold-text">金沢工業大学</b></p>
         </div>
     </footer>
