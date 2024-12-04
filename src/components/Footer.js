afc47ce1502c53dba82619d650d7cbbb303c1c64
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-04 18:34:24 +0900

    fix: より透明度を高く

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export const Footer = () => {
   }
 
   return (
-    <footer onClick={handleTransparent} style={{opacity: isTransparent ? 0.3 : 1, transition: "opacity 0.1s ease"}}>
+    <footer onClick={handleTransparent} style={{opacity: isTransparent ? 0.2 : 1, transition: "opacity 0.1s ease"}}>
         <div className="footer-nav">
             <p>&copy; {new Date().getFullYear()} <b className="bold-text">Sakamochanq</b></p>
             <p>Data and resources provided by <b className="bold-text">金沢工業大学</b></p>
