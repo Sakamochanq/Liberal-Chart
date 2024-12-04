c705f4c533f49335930e9a8f9e5135e0050fdcd2
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-04 18:29:57 +0900

    add: footerの表示非表示機能の実装

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Footer = () => {
+  const [isTransparent, setIsTransparent] = useState(false);
+
+  const handleTransparent = () => {
+    setIsTransparent(!isTransparent);
+  }
+
   return (
-    <footer>
+    <footer onClick={handleTransparent} style={{opacity: isTransparent ? 0.3 : 1, transition: "opacity 0.1s ease"}}>
         <div className="footer-nav">
             <p>&copy; {new Date().getFullYear()} <b className="bold-text">Sakamochanq</b></p>
             <p>Data and resources provided by <b className="bold-text">金沢工業大学</b></p>
