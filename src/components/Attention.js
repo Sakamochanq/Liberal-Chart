d866d0b6fb5cb930522089aca1b10f72b1c1bb2f
Sakamochanq <Sakamochan.q@outlook.jp>
2024-12-11 20:35:24 +0900

    add: 謝罪文の追加 [🥲]

--- src/components/Attention.js
+++ src/components/Attention.js
@@ -4,7 +4,9 @@ const Attention = () => {
   return (
     <div className="attention-container">
         <div className="attention-contents">
-            <img style={{margin:"40% 0px 20px 0px", pointerEvents:"none"}} width={"100px"} className="cry-logo" src="https://em-content.zobj.net/source/microsoft/310/smiling-face-with-tear_1f972.png"/>
+            <img style={{margin:"30% 0px 20px 0px", pointerEvents:"none"}} width={"100px"} className="cry-logo" src="https://em-content.zobj.net/source/microsoft/310/smiling-face-with-tear_1f972.png"/>
+            <br />
+            <h3>Oops... Sorry！</h3>
             <br />
             <b>Responsive of this size is not supported.</b>
             <p style={{marginTop:"10px"}}>このサイズのレスポンシブには対応していません。</p>
