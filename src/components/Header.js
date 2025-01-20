00f90a8438cef6d35ae84e9a5bb771e9ba05bf5c
Sakamochanq <Sakamochan.q@outlook.jp>
2025-01-20 23:13:47 +0900

    feat: ハンバーガーメニューの実装

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  document.body.addEventListener('click', (e) => {
+    if (e.target.closest('.menu-button') || e.target.closest('.menu-modal')) return;
+    setIsMenuOpen(false);
+  });
+
   return (
     <header>
       <h3 className="nav-logo">LC</h3>
-      <nav>
+      <button onClick={toggleMenu} className="menu-button">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav className={`menu-modal ${isMenuOpen ? 'open' : ''}`}>
         <ul className="menu">
           <li className="menu-item"><a href="#">Home</a></li>
+          <li className="menu-item"><a href="#">About</a></li>
           <li className="menu-item"><a href="https://github.com/Sakamochanq/Liberal-Chart" target="_blank">Help</a></li>
         </ul>
       </nav>
     </header>
-  )
-}
-export default Header
+  );
+};
+
+export default Header;
